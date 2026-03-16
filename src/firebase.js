3182a8505f597da3e60ad90d45411be2f59b14dd
wololo12340 <andrewz112233@gmail.com>
2026-03-16 13:19:52 -0700

    Updated website overall and tried implementing pages

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBdBRRCMS_Tp9bNoZoXXfX4I0FAHzk2HU",
+    authDomain: "react-firebase-app-ed720.firebaseapp.com",
+    projectId: "react-firebase-app-ed720",
+    storageBucket: "react-firebase-app-ed720.firebasestorage.app",
+    messagingSenderId: "381622174455",
+    appId: "1:381622174455:web:c40f234e08f2ffc35989f2"
+  };
+
+  const app = initializeApp(firebaseConfig);
